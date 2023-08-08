f020f608df92c5e90d4f70c9135fd7817ee0a2c8
Mateo Arismendy Uribe <Marismendyuribe4919@conestogac.on.ca>
2023-08-08 10:52:15 -0400

    about animation duration set to 0.008s

--- src/components/about/Hero.js
+++ src/components/about/Hero.js
@@ -29,7 +29,7 @@ const Hero = () => {
           y: 0,
           duration: 1.5,
           ease: Expo.easeInOut,
-          stagger: 0.015,
+          stagger: 0.008,
         }
       );
     }, container);
